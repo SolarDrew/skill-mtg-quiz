fb5e63a700ffbcbfe5ce8fb04616a19a9cd5f6c0
Drew Leonard <andy.j.leonard@gmail.com>
2018-07-14 19:12:21 +0100

    Better times

--- __init__.py
+++ __init__.py
@@ -4,12 +4,13 @@ import logging
 from opsdroid.matchers import match_regex, match_crontab
 from mtgsdk import Card
 
+
 def setup(opsdroid):
     logging.info("Loaded MtG Bot")
 
 
-@match_crontab('0 * * * *')
 @match_regex('give me a card', case_sensitive=False)
+@match_crontab('0 9,11,13,15,17,19 * * *', timezone='Europe/London')
 async def select_card(opsdroid, config, message):
     current_set = config['current_set']
     allcards = Card.where(set=current_set).all()
