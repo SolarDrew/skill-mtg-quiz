0e3c5c7aca1294a851ef48c6a663c54f25e9269d
Drew Leonard <andy.j.leonard@gmail.com>
2018-07-14 18:39:50 +0100

    Bugfix and change to once an hour

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 import random
 import logging
 
-from opsdroid.matchers import match_regex, match_cron
+from opsdroid.matchers import match_regex, match_crontab
 from mtgsdk import Card
 
 def setup(opsdroid):
     logging.info("Loaded MtG Bot")
 
 
-@match_cron('* * * * *')
+@match_crontab('0 * * * *')
 @match_regex('give me a card', case_sensitive=False)
 async def select_card(opsdroid, config, message):
     current_set = config['current_set']
